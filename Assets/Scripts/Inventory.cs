ec4db94270aa39c89a8d71277a5b089368c1b0e7
DevilRyu <43835556+DevilRyu@users.noreply.github.com>
2020-10-05 00:07:32 -0500

    Final commit

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+
+    //public int[] items;
+    //public gameobject[] slots;
+}
